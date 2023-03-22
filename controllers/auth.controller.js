c3f74351372f3e95c801eb881d58236329bdfb03
Piotr Sakowski <p.sakowski1213@gmail.com>
2023-03-22 14:24:11 +0100

    add function login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -8,6 +8,7 @@ exports.register = async (req, res) => {
 
     if (login && typeof login === 'string' && password && typeof password === 'string') {
       const userWithLogin = await User.findOne({ login });
+
       if (userWithLogin) {
         return res.status(409).send({ message: 'User with this login already exists' });
       }
@@ -24,5 +25,27 @@ exports.register = async (req, res) => {
 }
 
 exports.login = async (req, res) => {
+  try {
+    const { login, password } = req.body;
 
+    if (login && typeof login === 'string' && password && typeof password === 'string') {
+      return res.status(400).send({ message: 'Bad request' });
+    }
+
+    const user = await User.findOne({ login });
+
+    if (!user) {
+      res.status(400).send({ message: 'Login or password are incorrect' });
+    }
+    else {
+      if (bcrypt.compareSync(password, user.password)) {
+        res.status(200).send({ message: 'Login successful' });
+      }
+      else {
+        res.status(400).send({ message: 'Login or password are incorrect' });
+      }
+    }
+  } catch (err) {
+    res.status(500).send({ message: err.message })
+  }
 }
