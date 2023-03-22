83ae1323e915708fdbc485f345fc6a5278881377
Piotr Sakowski <p.sakowski1213@gmail.com>
2023-03-22 16:02:52 +0100

    format code

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,55 +1,52 @@
-const User = require('../models/User.model')
+const User = require('../models/User.model');
 const bcrypt = require('bcryptjs');
 
 exports.register = async (req, res) => {
   try {
-
     const { login, password, phone, avatar } = req.body;
 
-    if (login && typeof login === 'string' && password && typeof password === 'string') {
-      const userWithLogin = await User.findOne({ login });
-
-      if (userWithLogin) {
-        return res.status(409).send({ message: 'User with this login already exists' });
-      }
-
-      const user = await User.create({ login, password: await bcrypt.hash(password, 10), phone, avatar });
-      res.status(201).send({ message: 'User created' + user.login })
-    } else {
-      res.status(400).send({ message: 'Bad request' });
+    if (!login || typeof login !== 'string' || !password || typeof password !== 'string') {
+      return res.status(400).send({ message: 'Bad request' });
     }
 
+    const userWithLogin = await User.findOne({ login });
+
+    if (userWithLogin) {
+      return res.status(409).send({ message: 'User with this login already exists' });
+    }
+
+    const user = await User.create({ login, password: await bcrypt.hash(password, 10), phone, avatar });
+    res.status(201).send({ message: 'User created' + user.login });
   } catch (err) {
     res.status(500).send({ message: err.message });
   }
-}
+};
 
 exports.login = async (req, res) => {
   try {
-
     const { login, password } = req.body;
 
-    if (login && typeof login === 'string' && password && typeof password === 'string') {
-      const user = await User.findOne({ login });
-
-      if (!user) {
-        res.status(400).send({ message: 'Login or password are incorrect' });
-      }
-      else {
-        if (bcrypt.compareSync(password, user.password)) {
-          req.session.login = user.login;
-          res.status(200).send({ message: 'Login successful' });
-        }
-        else {
-          res.status(400).send({ message: 'Login or password are incorrect' });
-        }
-      }
+    if (!login || typeof login !== 'string' || !password || typeof password !== 'string') {
+      return res.status(400).send({ message: 'Bad request' });
     }
+
+    const user = await User.findOne({ login });
+
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      return res.status(400).send({ message: 'Login or password are incorrect' });
+    }
+
+    req.session.user = { login: user.login, id: user._id };
+    res.status(200).send({ message: 'Login successful' });
   } catch (err) {
-    res.status(500).send({ message: err.message })
+    res.status(500).send({ message: err.message });
   }
-}
+};
 
 exports.getUser = async (req, res) => {
-  res.send('Yeah! I\'m logged');
-}
+  if (!req.session.user || !req.session.user.login) {
+    return res.status(401).send({ message: 'You are not authorized!' });
+  }
+
+  res.send({ login: req.session.user.login });
+};
