42cab88eca9bdcb18146b1390f644c6cbf201282
Piotr Sakowski <p.sakowski1213@gmail.com>
2023-04-01 17:16:22 +0200

    add Home page

--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -1,7 +1,26 @@
-const Home = () => (
-  <div>
-    <h1>Home</h1>
-  </div>
-);
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAds, getAds } from '../../../redux/adsRedux';
+import AdBox from '../../features/AdBox/AdBox';
+
+const Home = () => {
+  const ads = useSelector(getAds);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAds());
+  }, [dispatch]);
+
+  return (
+    <div className='row my-4'>
+      {Array.isArray(ads) && ads.map(ad => (
+        <div key={ad._id} className='col-12 col-md-6 col-lg-4'>
+          <AdBox {...ad} />
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Home;
