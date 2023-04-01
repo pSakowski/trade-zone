9cc5d0e0457db1f60469a9624c8adf64c5d2b351
Piotr Sakowski <p.sakowski1213@gmail.com>
2023-04-01 17:11:33 +0200

    add Login & Logout button to NavBar

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -1,18 +1,15 @@
 import { useState } from 'react';
-import {
-  Button,
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink
-} from 'reactstrap';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavLink } from 'reactstrap';
+import { useSelector } from 'react-redux';
+import styles from './NavBar.module.scss';
+import { getUser } from '../../../redux/usersRedux';
+
+
 
 const NavBar = () => {
 
   const [isOpen, setIsOpen] = useState(false);
+  const isLoggedIn = useSelector(getUser);
 
   const toggle = () => {
     setIsOpen(!isOpen);
@@ -21,19 +18,21 @@ const NavBar = () => {
   return (
     <div>
       <Navbar color="dark" dark expand="md">
-        <NavbarBrand href="/">Bulletin Board</NavbarBrand>
+        <NavbarBrand href="/">
+          <span className={styles['navbar-brand']}>Bulletin Board</span>
+        </NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="justify-content-end flex-grow-1 pe-3" navbar>
-            <NavItem>
-              <NavLink href="/">Home</NavLink>
-            </NavItem>
+            <NavLink href="/">Home</NavLink>
+            {!isLoggedIn && <NavLink href="/login">Sign in</NavLink>}
+            {isLoggedIn && <NavLink href="/logout">Sign out</NavLink>}
+            {isLoggedIn && <NavLink href="/ad/add" className="btn btn-success ms-3">Add Ad</NavLink>}
           </Nav>
         </Collapse>
       </Navbar>
     </div>
   );
-
-}
+};
 
 export default NavBar;
