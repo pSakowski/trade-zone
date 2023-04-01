5caed68c6271accbb93bd8acd73c810cf97b0afd
Piotr Sakowski <p.sakowski1213@gmail.com>
2023-04-01 17:19:52 +0200

    moving files

---
+++ client/src/components/features/Logout/Logout.js
@@ -0,0 +1,31 @@
+import { API_URL } from '../../../config';
+import { logOut } from '../../../redux/usersRedux';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Logout = () => {
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const options = {
+      method: 'DELETE',
+      credentials: 'include',
+    };
+
+    fetch(`${API_URL}/auth/logout`, options)
+      .then(() => {
+        dispatch(logOut());
+        navigate('/');
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [dispatch, navigate]);
+
+  return null;
+}
+
+export default Logout;
--- client/src/components/pages/Ad/Ad.js
+++
@@ -1,7 +0,0 @@
-const Ad = () => (
-    <div>
-      <h1>Ad</h1>
-    </div>
-  );
-  
-  export default Ad;
