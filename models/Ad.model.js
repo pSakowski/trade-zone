b68921665513c07049aafdd44de702fd70c58148
Piotr Sakowski <p.sakowski1213@gmail.com>
2023-04-05 01:49:32 +0200

    change type  from string to object

--- models/Ad.model.js
+++ models/Ad.model.js
@@ -7,7 +7,7 @@ const adSchema = new mongoose.Schema({
   photo: { type: String, required: true },
   price: { type: String, required: true },
   location: { type: String, required: true },
-  seller: { type: String, ref: 'User', required: true }
+  seller: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
 });
 
 module.exports = mongoose.model('Ad', adSchema);
