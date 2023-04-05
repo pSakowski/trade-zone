1e30212cbec0bccabb01abc5c2271b707e20a4e5
Piotr Sakowski <p.sakowski1213@gmail.com>
2023-04-05 02:01:07 +0200

    correct display a single

--- client/src/components/features/Ad/Ad.js
+++ client/src/components/features/Ad/Ad.js
@@ -3,9 +3,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { fetchAds, getAdById } from '../../../redux/adsRedux';
 import styles from './Ad.module.scss';
+import { IMGS_URL } from '../../../config';
 
 const Ad = () => {
-  
   const { id } = useParams();
   const dispatch = useDispatch();
   const ad = useSelector(state => getAdById(state, id));
@@ -16,18 +16,20 @@ const Ad = () => {
 
   return (
     <div className={styles.adContainer}>
-      <div className={styles.adImage}>
-
-      </div>
-      <div className={styles.adInfo}>
-        <div>
-          <p className={styles.adDate}>Posted on {ad.date}</p>
+      <div className={styles.adDetails}>
+        <img className={styles.adPhoto} src={IMGS_URL + ad.photo} alt={ad.title} />
+        <div className={styles.adInfo}>
           <h2 className={styles.adTitle}>{ad.title}</h2>
           <p className={styles.adContent}>{ad.content}</p>
+          <p className={styles.adPrice}><span className={styles.price}>{ad.price} zł</span></p>
+          <p className={styles.adDate}><i className="fa fa-calendar"></i> {ad.location}, {ad.date}</p>
         </div>
-        <div>
-          <p className={styles.adPrice}>{ad.price} zł</p>
-          <p className={styles.adSeller}>Seller: {ad.seller}</p>
+      </div>
+      <div className={styles.sellerDetails}>
+        <img className={styles.sellerAvatar} src={IMGS_URL + ad.seller.avatar} alt={ad.seller.avatar} />
+        <div className={styles.sellerInfo}>
+          <p className={styles.sellerLogin}><i className="fa fa-user"></i> {ad.seller.login}</p>
+          <p className={styles.sellerPhone}><i className="fa fa-phone"></i> {ad.seller.phone}</p>
         </div>
       </div>
     </div>
