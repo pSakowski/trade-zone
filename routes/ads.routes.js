164db26a09ee9b4b2df691f6eb0d59c61358da37
Piotr Sakowski <p.sakowski1213@gmail.com>
2023-04-06 22:29:02 +0200

    remove authMiddleware in ads.routes

--- routes/ads.routes.js
+++ routes/ads.routes.js
@@ -8,7 +8,7 @@ router.get('/ads', ads.getAll);
 router.get('/ads/:id', ads.getById);
 router.post('/ads', imageUpload.single('photo'), ads.create);
 router.delete('/ads/:id', ads.delete);
-router.put('/ads/:id', authMiddleware, imageUpload.single('photo'), ads.update);
+router.put('/ads/:id', imageUpload.single('photo'), ads.update);
 router.get('/ads/search/:searchPhrase', ads.search);
 
 module.exports = router;
