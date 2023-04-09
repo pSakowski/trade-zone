066053ad83c7fae2ffe3a9acb0fb074500dcbfe5
Piotr Sakowski <p.sakowski1213@gmail.com>
2023-04-09 11:00:43 +0200

    fix

--- client/src/components/features/AdRemove/AdRemove.js
+++ client/src/components/features/AdRemove/AdRemove.js
@@ -17,7 +17,7 @@ const AdRemove = () => {
 
   const handleRemove = async () => {
     try {
-      const response = await axios.delete(`${API_URL}api/ad/remove/${id}`);
+      const response = await axios.delete(`${API_URL}api/ads/${id}`);
       if (response.status === 200) {
         setStatus('success');
         setTimeout(() => navigate('/'));
