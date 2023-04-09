4a54ec65d17e8a3fe583a99e02f58b02720aac62
Piotr Sakowski <p.sakowski1213@gmail.com>
2023-04-09 09:43:06 +0200

    fix

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
-export const API_URL = (process.env.NODE_ENV === 'production') ? '/' : 'https://bulletin-board-fullstack-1.psakowski1213.repl.co/';
+export const API_URL = (process.env.NODE_ENV === 'production') ? '/' : 'https://bulletin-board-fullstack.psakowski1213.repl.co/';
 
 export const IMGS_URL = (process.env.NODE_ENV === 'production') ? '/uploads/' : 'http://localhost:8000/uploads/';
